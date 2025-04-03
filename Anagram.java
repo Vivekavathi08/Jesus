8f4e1e3f6b916d82ed240b521b6b07d8d0885fc3
Vivekavathi08 <svivekavathi@gmail.com>
2025-04-03 20:02:08 +0530

    Jesus File added

---
+++ Anagram.java
@@ -0,0 +1,39 @@
+package exercise;
+
+import java.util.Arrays;
+
+public class Anagram {
+	 public static void main(String[] args) {
+		    String s1 = "jesus";
+		    String s2 = "Care";
+		    
+		    s1 = s1.toLowerCase();
+		    s2 = s2.toLowerCase();
+
+		   
+		    if(s1.length() == s2.length()) {
+
+		      char[] ch1 = s1.toCharArray();
+		      char[] ch2 = s2.toCharArray();
+
+		      
+		      Arrays.sort(ch1);
+		      Arrays.sort(ch2);
+
+		      
+		      boolean result = Arrays.equals(ch1, ch2);
+
+		      if(result) {
+		        System.out.println( "anagram.");
+		      }
+		      else {
+		        System.out.println("Not anagram.");
+		      }
+		    }
+		    else {
+		      System.out.println(" Not anagram.");
+		    }
+		  }
+		}
+
+
